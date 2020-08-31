dcc09b0972dc80e394c171e3ce3580c009fb3e63
wings <eternity.mr8@gmail.com>
2020-08-31 08:06:41 +0430

    max subset

--- Medium/Max Array Sum/index.php
+++ Medium/Max Array Sum/index.php
@@ -3,23 +3,15 @@
 
 function maxSubsetSum(int $n, array $arr)
 {
-    $sum = -INF;
-
-    $sumArr = [];
-    for ($step = $n - 1; $step >= 2 ; $step--) {
-        for ($i = 0; $i + $step < $n; $i++) {
-            $sumArr[$i] = $sumArr[$i] ?? [];
-            for ($j = $i + $step; $j < $n; $j+=$step) {
-                if($j - $i === $step){
-                    $sumArr[$i][$j] = (int)$arr[$i] + (int)$arr[$j];
-                }else{
-                    $sumArr[$i][$j] = $sumArr[$i][$j - $step] + (int)$arr[$j];
-                }
-                $sum = max($sum, $sumArr[$i][$j]);
-            }
-        }
+    $prev_prev = (int)$arr[0];
+    $prev = max($prev_prev, (int)$arr[1]);
+    for ($i = 2; $i < $n; $i++) {
+        $current = max((int)$arr[$i], $prev_prev + (int)$arr[$i], $prev);
+        $prev_prev = $prev;
+        $prev = $current;
     }
-    echo $sum;
+
+    echo $prev;
 }
 
 $n = readline();
